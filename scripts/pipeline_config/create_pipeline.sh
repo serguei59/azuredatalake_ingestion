5030e9e2e5a91169e9b3f023c69d99d12f67a1b9
Serge Buasa <sergebuasa@gmail.com>
2024-11-11 15:33:42 +0100

    redaction creation/configuration pipeline

--- scripts/pipeline_config/create_pipeline.sh
+++ scripts/pipeline_config/create_pipeline.sh
@@ -11,9 +11,54 @@ else
     echo ".env not found, please create one filled with required variables"
 fi
 
-# blob storage to adls2 container pipeline creation
+# Linked Services creation
+echo "Creation des Linked Services vers Blob Storage et Data Lake"
+az datafactory linked-service create --factory-name $DATA_FACTORY_NAME --resource-group $RESOURCE_GROUP --name BlobLinkedService --properties @pipeline/linked_service_blob.json
+if [ $? -eq 0 ]; then
+    echo "Blob storage Linked service created"
+else
+    echo "Failed to create Blob storage linked service"
+    exit 1
+fi
+
+az datafactory linked-service create --factory-name $DATA_FACTORY_NAME --resource-group $RESOURCE_GROUP --name DataLakeLinkedService --properties @pipeline/linked_service_datalake.json
+if [ $? -eq 0 ]; then
+    echo "Datalake linked-service created"
+else
+    echo "Failed to create Datalake linked-service"
+    exit 1
+fi
+
+# Datasets creation
+echo "Creation des Datasets des Blob Storage et Data Lake"
+az datafactory dataset create --factory-name $DATA_FACTORY_NAME --resource-group $RESOURCE_GROUP --name BlobDataset --properties @pipeline/dataset_blob.json
+if [ $? -eq 0 ]; then
+    echo "Blob storage dataset created"
+else
+    echo "Failed to create Blob storage dataset"
+    exit 1
+fi
+
+az datafactory dataset create --factory-name $DATA_FACTORY_NAME --resource-group $RESOURCE_GROUP --name DataLakeParquetDataset --properties @pipeline/dataset_datalake_parquet.json
+if [ $? -eq 0 ]; then
+    echo "Datalake parquet dataset created"
+else
+    echo "Failed to create Datalake parquet dataset"
+    exit 1
+fi
+
+# Blob storage to adls2 container adf's pipeline creation
 echo "Creating blob storage to data lake container pipeline..."
 az datafactory pipeline create \
     --factory-name $DATA_FACTORY_NAME \
     --name "BlobstorageToDataLakePipeline" \
-    --pipeline "$(cat pipeline_config/data_ingestion_pipeline.json)"
+    --pipeline "$(cat pipeline_config/data_ingestion_pipeline.json)" \
+    --resource_group $RESOURCE_GROUP
+
+if [ $? -eq 0 ]; then
+    echo "Data Factory Pipeline created"
+else
+    echo "Failed to create Data factory Pipeline"
+    exit 1
+fi
+
